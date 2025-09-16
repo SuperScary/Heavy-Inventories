80852760f42a51c315c61d6f4e209e43d3c975a8
SuperScary <wylanshoemaker@gmail.com>
2025-09-16 01:18:35 -0700

    Added event handlers for Forge. Weights and Player weights caching. Allow strength potions to increase the encumbrance threshold. Gui text for weights (Neo) Added client and server configs for NeoForge. Added Bracing enchantment for chestplates. Removed cloth-config api dependency.
    Known issue: When unequipping an armor piece with Bracing, the bracing modifier is still applied to the player max carry weight.

--- common/src/main/java/net/superscary/heavyinventories/mixin/PlayerMixin.java
+++ common/src/main/java/net/superscary/heavyinventories/mixin/PlayerMixin.java
@@ -2,7 +2,6 @@ package net.superscary.heavyinventories.mixin;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.superscary.heavyinventories.HeavyInventories;
 import net.superscary.heavyinventories.api.player.PlayerHolder;
 import net.superscary.heavyinventories.api.util.Functions;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +20,6 @@ public class PlayerMixin {
         if (holder.isEncumbered() || holder.isOverEncumbered()) {
             player.sendSystemMessage(Component.translatable("chat.heavyinventories.no_jump",
                     Component.translatable(Functions.either("chat.heavyinventories.over_encumbered", "chat.heavyinventories.encumbered", holder.isOverEncumbered()))));
-            HeavyInventories.LOGGER.info("{} tried to jump while encumbered! Weight at: {}% ({}/{})", player.getName().getString(), holder.getEncumberedPercentage(), holder.getWeight(), holder.getMaxWeight());
             ci.cancel();
         }
     }
